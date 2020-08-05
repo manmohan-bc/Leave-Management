40b3e9eb8d6783a63abba2a346f2ba7367daf00c
Manmohan Nayak <manmohan.bc@gmail.com>
2020-08-06 01:30:51 +0530

    Initial Commit

---
+++ Leave-Management/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Models
+{
+    public class DetailsLeaveTypeViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]
+        public string Name { get; set; }        
+    }
+}
